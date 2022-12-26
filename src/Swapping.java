90c001f038d04fc080a6aca6e3425c5d6382432b
lalitha <lalithaminni@gmail.com>
2022-12-26 18:22:07 +0530

    intial commit

---
+++ src/Swapping.java
@@ -0,0 +1,14 @@
+public class Swapping {
+
+    public static void main(String[] args) {
+        int a = 19;
+        int b = 75;
+        int c=b;//now c value is 5
+        b=a; //now b value is 10
+        a=c;//now a value is 5
+        System.out.println("a value after swapping is:" +a);// concatenation
+        System.out.println("b value after swapping is:" +b);
+
+    }
+
+}
